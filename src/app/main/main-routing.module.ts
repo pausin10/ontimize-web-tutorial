54520934565ac69883e1d3d32c62c2d45fa5cc9b
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2020-07-15 12:42:00 +0200

    Exercise 2

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -4,9 +4,14 @@ import { AuthGuardService } from 'ontimize-web-ngx';
 
 import { MainComponent } from './main.component';
 import { HomeModule } from './home/home.module';
+import { CustomersModule } from './customers/customers.module'
 
 export function loadHomeModule() {
   return HomeModule;
+}
+
+export function loadCustomersModule(){
+  return CustomersModule;
 }
 
 export const routes: Routes = [
@@ -19,6 +24,10 @@ export const routes: Routes = [
       {
         path: 'home',
         loadChildren: loadHomeModule
+      },
+      {
+        path: 'customers',
+        loadChildren : loadCustomersModule
       }
     ]
   }
